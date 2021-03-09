ab00a2cd995dec6efb2d36f2f04ef748fe72794d
Julian <julianrosenthal@yahoo.com>
2021-03-09 16:24:43 -0700

    moved global variables to new file

---
+++ globals.js
@@ -0,0 +1,10 @@
+// Add array prototype method random for use thoughout application
+Array.prototype.random = function() {
+  return this[Math.floor((Math.random()*this.length))]
+}
+// Define category and difficulty parameters for use with cocktail-trivia-api
+const difficulties = ['easy', 'medium', 'hard']
+const questionCategories = ['animals', 'art', 'books', 'celebrities', 'entertainment-board-games', 'entertainment-comics', 
+  'entertainment-film', 'entertainment-music', 'entertainment-musicals-theatres', 'entertainment-tv', 'entertainment-video-games', 
+  'general-knowledge', 'geography', 'history', 'mythology', 'politics', 'science-computers', 'science-gadgets', 'sports', 'vehicles']
+const questionField = document.getElementById('question')
