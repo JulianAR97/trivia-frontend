9add500eed151f17a2fab61723e5452d0695f46d
Julian <julianrosenthal@yahoo.com>
2021-03-09 16:23:18 -0700

    added timer file and moved relevant code

---
+++ timer.js
@@ -0,0 +1,14 @@
+const timerField = document.getElementById('timer')
+
+// Initialize timer here to prevent bug where timer gets multiplied
+let timer = setInterval(function() {
+  // Inner text will start with empty string, and become empty string again during answer check
+  if(timerField.innerText) {
+    timerField.innerText = parseFloat(timerField.innerText) - 1
+  }
+  timerColor()
+}, 1000)
+
+let timerColor = function() {
+  timerField.style.color = parseFloat(timerField.innerText) > 10 ? 'green' : 'red'
+}
