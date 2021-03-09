e871992bc68af0b23fe3ddd75e9b9f7f2990784a
Julian <julianrosenthal@yahoo.com>
2021-03-08 17:44:20 -0700

    added display question function

--- index.js
+++ index.js
@@ -7,13 +7,28 @@ const difficulties = ['easy', 'medium', 'hard']
 const questionCategories = ['animals', 'art', 'books', 'celebrities', 'entertainment-board-games', 'entertainment-comics', 
   'entertainment-film', 'entertainment-music', 'entertainment-musicals-theatres', 'entertainment-tv', 'entertainment-video-games', 
   'general-knowledge', 'geography', 'history', 'mythology', 'politics', 'science-computers', 'science-gadgets', 'sports', 'vehicles']
-
+const main = document.getElementById('main')
 
 let getQuestion = function() {
   // Get a random category and difficulty from previously defined constants
   let category = questionCategories.random()
   let difficulty = difficulties.random()
   let url = `http://cocktail-trivia-api.herokuapp.com/api/category/${category}/difficulty/${difficulty}/count/1`
-  fetch(url).then(resp => resp.json()).then(json => displayQuestion(json))
+  // We only need the first element of json because we are only handling one question at at time
+  fetch(url).then(resp => resp.json()).then(json => displayQuestion(json[0]))
 }
+
+let displayQuestion = function(question) {
+  // question object is in form {text: 'This value is actually the question', answers: 'array of answers'}
+  // answers array is in form [{correct: false, text: 'Answer Text'}, {correct: true, text: 'Answer Text'}, ..., ...]
+  let correctAnswer = question.answers.find(answer => answer.correct)
+  
+  // add question to main div
+  let p = document.createElement('p')
+  p.innerText = question.text
+  main.appendChild(p)
+  question.answers.forEach(a => appendAnswer(a))
+}
+
+
 
