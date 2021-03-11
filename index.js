b6dc5e8650d4f3129f0c481107e62798147d0d13
Julian <julianrosenthal@yahoo.com>
2021-03-11 15:35:52 -0700

    added start button and related functions

--- index.js
+++ index.js
@@ -11,4 +11,36 @@ function removeAllChildren(parentNode) {
 }
 // add a loading sign until content loaded
 
+let start = function() {
+  let b = document.createElement('button');
+  b.className = 'btn btn-primary centered';
+  b.id = 'start';
+  b.innerText = "Start";
+  main.appendChild(b)
+  b.addEventListener('click', loadContent)
+}
 
+
+let loadContent = function() {
+  let b = document.getElementById('start');
+  main.removeChild(b)
+  appendScoreAndTimer()
+  tryClass()
+}
+
+let appendScoreAndTimer = function() {
+  let pScore = document.createElement('p')
+  let pTimer = document.createElement('p')
+
+
+  pScore.innerHTML = 'Score: <span id="score">0</span>'
+  pTimer.innerHTML = 'Timer: <span id="timer"></span>'
+  
+  gameHelpers.appendChild(pScore);
+  gameHelpers.appendChild(pTimer);
+
+  // previously defined in globals
+  scoreField = document.getElementById('score');
+  timerField = document.getElementById('timer');
+
+}
