72243d73b3d773cf6d6196fc1521bfddad3d3e18
Julian <julianrosenthal@yahoo.com>
2021-03-20 13:54:44 -0700

    added annotations

--- javascripts/timer.js
+++ javascripts/timer.js
@@ -1,13 +1,14 @@
-// put in class
-// Initialize timer here to prevent bug where timer gets multiplied
+// for use with setInterval called in question class
 let timerPaused = true;
 let timeLeft = 30;
 
 let timerCountdown = function() {
+  // Decrement timer by 1 when timer is not paused
   if (!timerPaused) {
     timerField.innerText = timeLeft
     timeLeft -= 1
 
+    // Can only be activated if the timer is not paused and time is 0
     if (timeLeft === 0 ) {
       console.log('here')
       timerPaused = true;
