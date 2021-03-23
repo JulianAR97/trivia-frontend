ddee243ef46950a6384a5a8082cedea741a61675
Julian <julianrosenthal@yahoo.com>
2021-03-23 14:09:51 -0700

    added more global variables

--- javascripts/globals.js
+++ javascripts/globals.js
@@ -3,14 +3,32 @@ Array.prototype.random = function() {
   return this[Math.floor((Math.random()*this.length))]
 }
 // Define category and difficulty parameters for use with cocktail-trivia-api
-const difficulties = ['easy', 'medium', 'hard']
-const questionCategories = [
-  'animals', 'art', 'celebrities', 'entertainment-board-games', 'entertainment-books', 
-  'entertainment-comics', 'entertainment-film', 'entertainment-music', 'entertainment-musicals-theatres', 
-  'entertainment-tv', 'entertainment-video-games', 'general-knowledge', 'geography', 'history', 'mythology', 
-  'politics', 'science-computers', 'science-gadgets', 'sports', 'vehicles'
-]
 
+// Keys are categories and values are api endpoints
+const questionCategories = {
+  'Animals': 'animals',
+  'Art': 'art',
+  'Board Games': 'entertainment-board-games',
+  'Books': 'entertainment-books',
+  'Celebrities': 'celebrities',
+  'Comics': 'entertainment-comics',
+  'Computers': 'science-computers',
+  'Film': 'entertainment-film',
+  'Gadgets': 'science-gadgets',
+  'General Knowledge': 'general-knowledge',
+  'Geography': 'geography',
+  'History': 'history',
+  'Music': 'entertainment-music',
+  'Musicals': 'entertainment-musicals-theatres',
+  'Mythology': 'mythology',
+  'Politics': 'politics',
+  'Sports': 'sports',
+  'T.V.': 'entertainment-tv',
+  'Vehicles': 'vehicles',
+  'Video Games': 'entertainment-video-games',
+}
+
+const apiURL = 'http://cocktail-trivia-api.herokuapp.com/api/category/'
 const customMaroon = '#843b62';
 const customSalmon = '#ffb997';
 const body = document.getElementsByTagName('body')[0]
@@ -21,7 +39,8 @@ const questionField = document.getElementById('question')
 const answerField = document.getElementById('answers')
 let scoreField;
 let timerField;
+let questionList;
+let selectedCategory;
 
 // cool js stuff
-difficulties.random() = difficulties[Math.floor(Math.random() * difficulties.length)]
-questionCategories.random() = questionCategories[Math.floor(Math.random() * questionCategories.length)]
+
