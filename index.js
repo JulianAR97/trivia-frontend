2c9020321bcf2b165ee42df2012fdc9220d093f4
Julian <julianrosenthal@yahoo.com>
2021-03-23 17:18:55 -0700

    moved functions to helpers.js

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-function removeAllChildren(parentNode) {
-  while (parentNode.firstChild) {
-    parentNode.removeChild(parentNode.firstChild)
-  }
-}
 // add a loading sign until content loaded
 
 let start = function() {
   listCategories();
 }
 
+// Add 'links' for all question categories and append to DOM
 let listCategories = function() {
   // key is category itself
   for (let key in questionCategories) {
@@ -22,50 +18,6 @@ let listCategories = function() {
   }
 }
 
-let getQuestions = function(category) {
-  // Get endpoint from questionCategories Object in globals and append it to apiURL in globals
-  let endpoint = questionCategories[category]
-  fetch(apiURL + endpoint)
-    .then(resp => resp.json())
-    .then(json => {
-      setQuestions(json)
-      q = new Question;
-      q.getQuestion()
-    })
-}
-
-// Add button style in js because can't override bootstrap in css file
-let addButtonStyle = function(button) {
-  button.className = 'btn btn-primary';
-  button.style.backgroundColor = 'transparent';
-  // maroon color 
-  button.style.color = customMaroon;
-  button.style.borderColor = customMaroon;
-  button.style.borderRightWidth = '3px';
-  button.style.borderBottomWidth = '3px';
-  return button
-}
-
-let setQuestions = function(json) {
-  questionList = json
-  console.log(questionList)
-}
-
-let removeCategoriesFromMain = function(categories, leftDiv) {
-  // If the questions are in the main div, move them to left div
-  if (!!document.querySelectorAll("#main > p.question-category")[0]) {
-    categories.forEach(category => {
-      main.removeChild(category);
-      leftDiv.appendChild(category);
-      leftDiv.appendChild(document.createElement('br'))
-    }) 
-    appendScoreAndTimer()
-  } else {
-    removeAllChildren(questionField)
-    removeAllChildren(answerField)
-  }
-}
-
 let loadContent = function(e) {
   let categories = document.querySelectorAll('p.question-category');
   let leftDiv = document.querySelector("body > div.row > div:nth-child(1)");
@@ -74,56 +26,16 @@ let loadContent = function(e) {
 
   // Save to variable defined in globals for later use;
   selectedCategory = e.target.innerText;
-  getQuestions(selectedCategory)
+  getQuestions(selectedCategory) // helpers.js
 }
 
-let appendScoreAndTimer = function() {
-  let pScore = document.createElement('p')
-  let pTimer = document.createElement('p')
-
-
-  pScore.innerHTML = '<h4>Score: <span id="score">0</span></h4>'
-  pTimer.innerHTML = '<h4>Timer: <span id="timer"></span></h4>'
-  
-  gameHelpers.appendChild(pScore);
-  gameHelpers.appendChild(pTimer);
-
-  // previously defined in globals
-  scoreField = document.getElementById('score');
-  timerField = document.getElementById('timer');
-
-}
-
-let removeCategories = function() {
-  let breaks = document.querySelectorAll('div#main > br')
-  breaks.forEach(br => main.removeChild(br));
-  let leftDiv = document.querySelector("body > div.row > div:nth-child(1)")
-  removeAllChildren(leftDiv)
-}
 let gameOver = function() {
-  removeCategories()
+  removeCategories() // helpers.js
   let finalScore = scoreField.innerText
-  removeAllChildren(questionField)
-  removeAllChildren(gameHelpers)
-  removeAllChildren(answerField)
+  removeQuestionAndAnswers() //helpers.js
+  removeAllChildren(gameHelpers) //helpers.js
   Score.fetchTopTenScores(finalScore)
 }
-
-let startGameHTML = function() {
-  // First, remove left and right divs created during endgame
-  [document.getElementById('left-div'), document.getElementById('right-div')].forEach(div => {
-    main.removeChild(div)
-  })
-  
-  // Then reset div classnames for divs not used during endgame
-  document.querySelectorAll('div.no-class').forEach(div => {
-    div.className = 'col-sm-3'
-  });
-  
-  // Change main className back
-  main.className = 'col-sm-6'
-}
-
 
 let resetGame = function() {
   // After sending score to back end, remove table, and start over
---
+++ javascripts/helpers.js
@@ -0,0 +1,94 @@
+let removeAllChildren = function(parentNode) {
+  while (parentNode.firstChild) {
+    parentNode.removeChild(parentNode.firstChild)
+  }
+}
+
+let removeQuestionAndAnswers = function() {
+  removeAllChildren(questionField)
+  removeAllChildren(answerField)
+}
+
+// Add button style in js because can't override bootstrap in css file
+let addButtonStyle = function(button) {
+  button.className = 'btn btn-primary';
+  button.style.backgroundColor = 'transparent';
+  // maroon color 
+  button.style.color = customMaroon;
+  button.style.borderColor = customMaroon;
+  button.style.borderRightWidth = '3px';
+  button.style.borderBottomWidth = '3px';
+  return button
+}
+
+let removeCategories = function() {
+  let breaks = document.querySelectorAll('div#main > br')
+  breaks.forEach(br => main.removeChild(br));
+  let leftDiv = document.querySelector("body > div.row > div:nth-child(1)")
+  removeAllChildren(leftDiv)
+}
+
+let appendScoreAndTimer = function() {
+  let pScore = document.createElement('p')
+  let pTimer = document.createElement('p')
+
+
+  pScore.innerHTML = '<h4>Score: <span id="score">0</span></h4>'
+  pTimer.innerHTML = '<h4>Timer: <span id="timer"></span></h4>'
+  
+  gameHelpers.appendChild(pScore);
+  gameHelpers.appendChild(pTimer);
+
+  // previously defined in globals
+  scoreField = document.getElementById('score');
+  timerField = document.getElementById('timer');
+
+}
+
+
+let getQuestions = function(category) {
+  // Get endpoint from questionCategories Object in globals and append it to apiURL in globals
+  let endpoint = questionCategories[category]
+  fetch(apiURL + endpoint)
+    .then(resp => resp.json())
+    .then(json => {
+      setQuestions(json)
+      q = new Question;
+      q.getQuestion()
+    })
+}
+
+let setQuestions = function(json) {
+  questionList = json // defined in globals
+  console.log(questionList)
+}
+
+
+let startGameHTML = function() {
+  // First, remove left and right divs created during endgame
+  [document.getElementById('left-div'), document.getElementById('right-div')].forEach(div => {
+    main.removeChild(div)
+  })
+  
+  // Then reset div classnames for divs not used during endgame
+  document.querySelectorAll('div.no-class').forEach(div => {
+    div.className = 'col-sm-3'
+  });
+  
+  // Change main className back
+  main.className = 'col-sm-6'
+}
+
+let removeCategoriesFromMain = function(categories, leftDiv) {
+  // If the questions are in the main div, move them to left div
+  if (!!document.querySelectorAll("#main > p.question-category")[0]) {
+    categories.forEach(category => {
+      main.removeChild(category);
+      leftDiv.appendChild(category);
+      leftDiv.appendChild(document.createElement('br'))
+    }) 
+    appendScoreAndTimer() // helpers.js
+  } else {
+    removeQuestionAndAnswers() // helpers.js
+  }
+}
